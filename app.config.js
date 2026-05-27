eb24f8e70ca10e78e7896689823718b6012d8104
sachcha99 <sachcha99@gmail.com>
2026-05-28 07:59:56 +1000

    remove react-native-google-mobile-ads (new arch incompatible with current build)

--- app.config.js
+++ app.config.js
@@ -3,6 +3,9 @@ const appJson = require('./app.json');
 module.exports = ({ config }) => ({
   ...appJson.expo,
   extra: {
+    eas: {
+      projectId: '555e43bf-9ed8-4f01-8d85-61d5f6dea41e',
+    },
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || '',
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || '',
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || '',
@@ -18,19 +21,9 @@ module.exports = ({ config }) => ({
     ADMOB_BANNER_UNIT_ID:
       process.env.ADMOB_BANNER_UNIT_ID ||
       'ca-app-pub-3940256099942544/6300978111',
+    WHISPER_API_KEY: process.env.WHISPER_API_KEY || null,
   },
   plugins: [
     ...appJson.expo.plugins,
-    [
-      'react-native-google-mobile-ads',
-      {
-        androidAppId:
-          process.env.ADMOB_ANDROID_APP_ID ||
-          'ca-app-pub-3940256099942544~3347511713',
-        iosAppId:
-          process.env.ADMOB_IOS_APP_ID ||
-          'ca-app-pub-3940256099942544~1458002511',
-      },
-    ],
   ],
 });
